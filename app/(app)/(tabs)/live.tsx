aa97d6e0704d93ee8e4f9343e0f07bf4cd84e0f9
jitunayak <jitunayak715@gmail.com>
2025-02-10 01:30:47 +0530

    Added Button, ScrollView; updated snapPoints, styles

--- app/(app)/(tabs)/live.tsx
+++ app/(app)/(tabs)/live.tsx
@@ -2,12 +2,14 @@ import AssetCompactCard from "@/src/components/AssetCompactCard";
 import Title from "@/src/components/Title";
 import { Colors } from "@/src/Constant";
 import { wishListedAuctions } from "@/src/data/auctions";
+import Button from "@/src/ui/Button";
 import { HStack } from "@/src/ui/HStack";
 import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
 import React, { useCallback, useRef } from "react";
 import {
   FlatList,
   SafeAreaView,
+  ScrollView,
   Text,
   TouchableOpacity,
   View,
@@ -44,7 +46,7 @@ export default function Live() {
         <BottomSheet
           ref={bottomSheetRef}
           onChange={handleSheetChanges}
-          snapPoints={["10%, 50%, 90%"]}
+          snapPoints={["10%, 50%, 100%"]}
         >
           <BottomSheetView style={{ flex: 1, padding: 2 }}>
             <Text
@@ -58,42 +60,48 @@ export default function Live() {
               Filters
             </Text>
 
-            <View style={{ gap: 16, paddingHorizontal: 8, marginBottom: 16 }}>
-              <Title value="Category" />
-
-              <HStack justifyContent="flex-start">
-                {["vehicles", "lands", "properties", "gold"].map((item) => (
-                  <TouchableOpacity
-                    style={{
-                      backgroundColor: Colors.background,
-                      padding: 10,
-                      borderRadius: 16,
-                      marginBottom: 8,
-                      borderWidth: 0.6,
-                      borderColor: Colors.border,
-                      marginRight: 8,
-                    }}
-                  >
-                    <Text
+            <ScrollView>
+              <View style={{ gap: 16, paddingHorizontal: 8 }}>
+                <Title value="Type" />
+                <HStack justifyContent="flex-start">
+                  {["vehicles", "lands", "properties", "gold"].map((item) => (
+                    <TouchableOpacity
                       style={{
-                        color: Colors.text,
+                        backgroundColor: Colors.background,
+                        padding: 12,
+                        borderRadius: 8,
+                        marginBottom: 8,
+                        borderWidth: 1,
+                        borderColor:
+                          item === "vehicles" ? Colors.primary : Colors.border,
+                        marginRight: 8,
                       }}
                     >
-                      {item}
-                    </Text>
-                  </TouchableOpacity>
-                ))}
-              </HStack>
+                      <Text
+                        style={{
+                          color:
+                            item === "vehicles" ? Colors.primary : Colors.text,
+                        }}
+                      >
+                        {item}
+                      </Text>
+                    </TouchableOpacity>
+                  ))}
+                </HStack>
 
-              <Title value="Price Range" />
-              <View style={{ flexDirection: "row", flexWrap: "wrap" }}>
-                {["< 10000", "100000-100000", "100000-300000", "> 300000"].map(
-                  (item) => (
+                <Title value="Price Range" />
+                <View style={{ flexDirection: "row", flexWrap: "wrap" }}>
+                  {[
+                    "< 10000",
+                    "100000-100000",
+                    "100000-300000",
+                    "> 300000",
+                  ].map((item) => (
                     <TouchableOpacity
                       style={{
                         backgroundColor: Colors.background,
                         padding: 10,
-                        borderRadius: 16,
+                        borderRadius: 8,
                         marginBottom: 8,
                         borderColor: Colors.border,
                         borderWidth: 0.6,
@@ -108,43 +116,52 @@ export default function Live() {
                         {item}
                       </Text>
                     </TouchableOpacity>
-                  )
-                )}
-              </View>
-              <Title value="Location" style={{ marginBottom: 16 }} />
-              <View style={{ flexDirection: "row", flexWrap: "wrap" }}>
-                {[
-                  "Odisha",
-                  "Kolkata",
-                  "Delhi",
-                  "Mumbai",
-                  "Pune",
-                  "Chennai",
-                  "Hyderabad",
-                  "Bangalore",
-                ].map((item) => (
-                  <TouchableOpacity
-                    style={{
-                      backgroundColor: Colors.background,
-                      padding: 10,
-                      borderRadius: 16,
-                      marginBottom: 8,
-                      borderWidth: 0.6,
-                      borderColor: Colors.border,
-                      marginRight: 8,
-                    }}
-                  >
-                    <Text
+                  ))}
+                </View>
+                <Title value="Location" style={{ marginBottom: 16 }} />
+                <View style={{ flexDirection: "row", flexWrap: "wrap" }}>
+                  {[
+                    "Odisha",
+                    "Kolkata",
+                    "Delhi",
+                    "Mumbai",
+                    "Pune",
+                    "Chennai",
+                    "Hyderabad",
+                    "Bangalore",
+                  ].map((item) => (
+                    <TouchableOpacity
                       style={{
-                        color: Colors.text,
+                        backgroundColor: Colors.background,
+                        padding: 10,
+                        borderRadius: 8,
+                        marginBottom: 8,
+                        borderWidth: 0.6,
+                        borderColor:
+                          item === "Odisha" ? Colors.primary : Colors.border,
+                        marginRight: 8,
                       }}
                     >
-                      {item}
-                    </Text>
-                  </TouchableOpacity>
-                ))}
+                      <Text
+                        style={{
+                          color:
+                            item === "Odisha" ? Colors.primary : Colors.text,
+                        }}
+                      >
+                        {item}
+                      </Text>
+                    </TouchableOpacity>
+                  ))}
+                </View>
               </View>
-            </View>
+            </ScrollView>
+            <Button
+              title="Apply Filter"
+              variant="primary"
+              onPress={() => {
+                bottomSheetRef.current?.collapse();
+              }}
+            />
           </BottomSheetView>
         </BottomSheet>
       </View>
